3234adff45dc3702b53df58fa5d056a34a64d1ec
whizkelvin <kelvinmelchizedek@gmail.com>
2025-11-11 01:16:05 +0000

    corousel done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,46 @@ import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
 import { useAuth } from "../AuthContext";
 
+import Slider from "react-slick";
 
 const Home = () => {
 const navigate = useNavigate();
   const { session } = useAuth();
 
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   const handleSignOut = async () => {
     await supabase.auth.signOut();
     navigate("/");
@@ -15,23 +50,39 @@ const navigate = useNavigate();
   console.log(session);
 
   return (
-    <div className="min-h-screen bg-green-50 flex flex-col justify-center items-center">
+    <div className="">
      
      
      
 
-      {session && (
+      <div className="slider-container">
+      <Slider {...settings}>
         <div>
-          <h3>Welcome Back</h3>
-          <p>Role: {session.user.user_metadata.role}</p>
-          <p> Name: {session.user.user_metadata.name}</p>
-          <p>Email: {session.user.email}</p>
-          <p>Programme: {session.user.user_metadata.program}</p>
-          <p>Id: {session.user.user_metadata.student_id}</p>
+          <h3><img src="https://res.cloudinary.com/dnkk72bpt/image/upload/v1762440610/Regent-University-College-of-Science-and-Technology-Mallam-Ghana-SchoolFinder-TortoisePathcom_himnme.jpg" alt="" /></h3>
         </div>
-      
-       
-      )}
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+      </Slider>
+    </div>
 
       <button
         onClick={handleSignOut}
